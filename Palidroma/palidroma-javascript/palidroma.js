3246ad1f198e9d9f0d56f8f880a57317d5eca288
EneaSema <sema.enea1993@gmail.com>
2025-04-07 16:07:19 +0200

    write -base-exercise-one

--- Palidroma/palidroma-javascript/palidroma.js
+++ Palidroma/palidroma-javascript/palidroma.js
@@ -1,5 +1,20 @@
-console.log("ciao");
+// console.log("ciao");
 
 // Palidroma
 // 1 Chiedere all’utente di inserire una parola
 // 2 Creare una funzione per capire se la parola inserita è palindroma
+
+const requestWordUser = "otto";
+
+const 
+
+function verificatedWord(requestWordUser) {
+  for (let i = 0; i < requestWordUser.length; i++) {
+    let currentCharacter = currentCharacter.push(requestWordUser[i]);
+  }
+  if (currentCharacter === requestWordUser) {
+    console.log("Parola palindroma");
+  } else {
+    console.log("Parola non palidroma");
+  }
+}
